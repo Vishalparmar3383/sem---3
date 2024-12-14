d31ffeb9673803a6db5496bdbbffd895e18ce3ab
Vishal Parmar <143985550+Vishalparmar3383@users.noreply.github.com>
2024-12-14 15:58:26 +0530

    Add files via upload

---
+++ CircleAreaCalculator.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class CircleAreaCalculator{
+	public static void main(String[] args) {
+		Scanner scanner= new Scanner(System.in);
+
+		System.out.print("Enter the radius of circle");
+		double radius= scanner.nextDouble();
+
+		double area= 3.414*radius*radius;
+		System.out.println("The area of the circle with given radius is"+area);
+		scanner.close();
+	}
+}
